592f124de816b5e09ff04358b04f13a92c9b772f
Jean-Baptiste Kaiser <jb@marmelab.com>
2022-09-20 15:52:26 +0200

    handle no comments

--- src/comments-component/MyRecordTimeline.tsx
+++ src/comments-component/MyRecordTimeline.tsx
@@ -5,36 +5,32 @@ import {
   useRecordEvents,
   UseRecordEventsOptions,
 } from "@react-admin/ra-audit-log";
-import { Box, Card } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 import { MyRecordTimelineItem } from "./MyRecordTimelineItem";
 
 export const MyRecordTimeline = (props: MyRecordTimelineProps) => {
   const { data, isLoading } = useRecordEvents(props);
 
-  const {
-    page,
-    perPage,
-    sort,
-    order,
-    record,
-    resource,
-    skeleton = <>"Loading..."</>,
-    ...rest
-  } = props;
+  const { page, perPage, sort, order, record, resource, skeleton, ...rest } =
+    props;
 
   return (
     <Box sx={{ ml: 2, minWidth: 300 }}>
       <Card sx={{ p: 2 }}>
-        <Timeline
-          isLoading={isLoading}
-          records={data}
-          skeleton={skeleton}
-          {...rest}
-        >
-          <TimelineGroup>
-            <MyRecordTimelineItem />
-          </TimelineGroup>
-        </Timeline>
+        {!isLoading && !data?.length ? (
+          <Typography>There are no comments yet</Typography>
+        ) : (
+          <Timeline
+            isLoading={isLoading}
+            records={data}
+            skeleton={skeleton}
+            {...rest}
+          >
+            <TimelineGroup>
+              <MyRecordTimelineItem />
+            </TimelineGroup>
+          </Timeline>
+        )}
       </Card>
     </Box>
   );
